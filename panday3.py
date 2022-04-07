869e20a067bb00573baaf3c63f378b2394e85f65
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2022-04-07 12:10:23 +0530

    pandas intro

--- panday3.py
+++ panday3.py
@@ -1,4 +1,21 @@
 import pandas as pd
+
+print("pandas means panelData(roes,col)")
+print("used for data analysis,manuplations and cleaning ")
+print("2data structures we useseries and  datafarame")
+print("-------------------------------------------------------")
+print("to check version")
+print(pd.__version__)
+print("--------------------------------------------------------")
+print("to check available methods for panadas")
+print(dir(pd))
+print("---------------------------------------------------------")
+print("series:sequence data")
+str="hello kamalatha"
+print(str,type(str))#string type
+print(type(pd.Series(str)))
+print(pd.Series(str)) #object type
+print("----------------------------------------------------------")
 table={'int':[9,87,90,1004,34],'float':[9.6,45.4,34.76,45.3,5.6],'str':['abc','def','ghi','jkl','mno']}
 frame=pd.DataFrame(table)
 print(frame)
